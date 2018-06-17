7074d9594ee75ac7ed6ec2e42d4ce720ce0fc5c8
pigpigever <1690384523@qq.com>
2018-06-17 11:12:44 +0800

    添加登录界面的绘制

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,17 @@ export default {
 </script>
 
 <style>
-#app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+html, body, #app {
+	width: 100%;
+	height: 100%;
+}
+body {
+	/* font-family: 'Source Sans Pro', sans-serif; */
+	font-family: 宋体;
+	color: white;
+	font-weight: 300;
+}
+*{
+	box-sizing: border-box;
 }
 </style>
